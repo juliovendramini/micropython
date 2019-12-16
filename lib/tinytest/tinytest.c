juliovendramini/micropython

commit f4ed2dfa942339dc1f174e8a83ff0d41073f1972

lib/tinytest: Clean up test reporting in the presence of stdout output.
tinytest is written with the idea that tests won't write to stdout, so it
prints test name witjout newline, then executes test, then writes status.
But MicroPython tests write to stdout, so the test output becomes a mess.
So, instead print it like:

    # starting basics/andor.py
    ... test output ...
    basics/andor.py: OK

lib/tinytest/tinytest.c
@@ -234,8 +234,9 @@ testcase_run_one(const struct testgroup_t *group,
 		return SKIP;
 	}
 
+	printf("# starting %s%s\n", group->prefix, testcase->name);
 	if (opt_verbosity>0 && !opt_forked) {
-		printf("%s%s: ", group->prefix, testcase->name);
+		//printf("%s%s: ", group->prefix, testcase->name);
 	} else {
 		if (opt_verbosity==0) printf(".");
 		cur_test_prefix = group->prefix;
@@ -252,6 +253,7 @@ testcase_run_one(const struct testgroup_t *group,
 		outcome = testcase_run_bare_(testcase);
 	}
 
+	printf("%s%s: ", group->prefix, testcase->name);
 	if (outcome == OK) {
 		++n_ok;
 		if (opt_verbosity>0 && !opt_forked)
@@ -263,7 +265,8 @@ testcase_run_one(const struct testgroup_t *group,
 	} else {
 		++n_bad;
 		if (!opt_forked)
-			printf("\n  [%s FAILED]\n", testcase->name);
+			//printf("\n  [%s FAILED]\n", testcase->name);
+			puts("FAILED");
 	}
 
 	if (opt_forked) {

commit 30debbbd97a922cdd43017acf3999f5de421cde4

lib/tinytest: fix result not printed on newline
When a test prints output during a test, it does so by adding a newline to the beginning of the line, rather than the end. So we need to do the same when printing the final result of the test, otherwise it ends up being appended to the end of the last message, which makes it hard to read.

Fixes: f4ed2dfa942339dc1f174e8a83ff0d41073f1972

lib/tinytest/tinytest.c
@@ -253,7 +253,7 @@ testcase_run_one(const struct testgroup_t *group,
 		outcome = testcase_run_bare_(testcase);
 	}
 
-	printf("%s%s: ", group->prefix, testcase->name);
+	printf("\n%s%s: ", group->prefix, testcase->name);
 	if (outcome == OK) {
 		++n_ok;
 		if (opt_verbosity>0 && !opt_forked)