d334338d2c9c96546029126aa9b2bd2bffeb6097
David Lechner <david@pybricks.com>
2020-05-16 18:40:17 -0500

    Revert "pybricks: override sys.implementation"
    This reverts commit 319b962e2f96bf09039aa7d5f4d8997ff755bd5a.

--- tests/basics/sys1.py
+++ tests/basics/sys1.py
@@ -14,7 +14,7 @@ except AttributeError:
     print(True)
 
 try:
-    print(sys.implementation.name in ('cpython', 'pybricks-micropython'))
+    print(sys.implementation.name in ('cpython', 'micropython'))
 except AttributeError:
     # Effectively skip subtests
     print(True)
